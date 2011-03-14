ee82a3ae89db507faca3747556713cb483e2a538
Manu Bhadoria <manubhadoria@localhost.(none)>
2011-03-14 12:32:53 -0700

    =Keep alive thread added

---
+++ keepAliveTimer.cc
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+#include<map>
+
+#include "main.h"
+#include "keepAliveTimer.h"
+
+void *keepAliveTimer_thread(void *arg){
+
+while(1)
+{
+	sleep(1);
+
+	for (map<int, struct connectionNode>::iterator it = connectionMap.begin(); it != connectionMap.end(); ++it){
+	
+		if((*it).second.keepAliveTimer > 0)
+			(*it).second.keepAliveTimer--;
+		else
+		{
+			struct Message mes;
+			mes.type = 0xf8;
+			printf("Sent Keep Alive Message to: %d\n", (*it).first);
+			pushMessageinQ((*it).first, mes);
+		}
+	}
+}
+return 0;
+}
+
