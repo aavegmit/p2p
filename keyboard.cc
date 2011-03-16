5a1525fc683322459529d1ee3f11b65e41ab5afa
Manu Bhadoria <manubhadoria@localhost.(none)>
2011-03-15 23:18:22 -0700

    = not much though

--- keyboard.cc
+++ keyboard.cc
@@ -3,15 +3,29 @@
 #include <iostream>
 #include <string.h>
 #include <stdlib.h>
+#include "iniParser.h"
+#include "signalHandler.h"
 
 void *keyboard_thread(void *arg){
 	char *inp = new char[512] ;
 	memset(inp, '\0', 512) ;	
 	while(1){
-		printf("servant:PORT> ") ;
+		printf("servant:%d> ", myInfo->portNo) ;
 		fgets(inp, 511, stdin) ;
 
-
+		if(strcasecmp(inp, "shutdown")==0)
+		{
+			//kill(node_pid, SIGTERM);
+			//break;
+		}
+		else if(strcasecmp(inp, "status neighbors")==0)
+		{
+			printf("Under construction....\n");
+		}
+		else
+		{
+			
+		}
 
 		memset(inp, '\0', 512) ;	
 	}
