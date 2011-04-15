c72db119197f1fcb723b1f809376b59c78e141b8
Manu Bhadoria <manubhadoria@localhost.(none)>
2011-04-15 16:26:31 -0700

    = get command line done

--- main.h
+++ main.h
@@ -130,6 +130,7 @@ extern int softRestartFlag ;
 extern int globalFileNumber ;
 extern int globalSearchCount ;
 extern int currentCacheSize;
+extern unsigned char extFile[256];
 extern FILE *f_log;
 extern map<int, struct connectionNode> connectionMap ;				// Stores all the info related to a connection
 extern list<pthread_t > childThreadList ;
